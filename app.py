a602e9fdf790ac1ce19e65d26f97fefb170eed3b
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2026-09-19 12:55:26 +0530

    feat: dummy UI added

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ import pandas as pd
 import numpy as np
 
 st.title("FeedbackIQ")
+
+import streamlit as st
+
+pages = [
+    st.Page("pages/streaming.py", title="Feedback Streaming", icon="🚀"),
+    st.Page("pages/agent.py", title="Agent", icon="🤖"),
+    st.Page("pages/analytics.py", title="Insights", icon="📊"),
+]
+
+pg = st.navigation(pages, position="top")
+
+pg.run()
